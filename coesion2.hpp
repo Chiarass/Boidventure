1a817cdba000add7c5f8aeb4b015a280f08e0437
Chiara Baldelli <chiarybaldelli@gmail.com>
2023-05-27 19:08:36 +0200

    Added a header with SFML graphic

---
+++ coesion2.hpp
@@ -0,0 +1,12 @@
+#ifndef COESION_HPP
+#define COESION_HPP
+#include <SFML/Graphics.hpp>
+#include <algorithm>
+#include <numeric>
+#include <random>
+#include <vector>
+
+const width = 800;
+const height = 600;
+
+#endif
