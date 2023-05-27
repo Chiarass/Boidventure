5e9c7483208310ceb2719f6bf3bd949fbcd108dd
Chiara Baldelli <chiarybaldelli@gmail.com>
2023-05-27 04:25:38 +0200

    Creation of the header and the main file

---
+++ coesion.hpp
@@ -0,0 +1,43 @@
+#ifndef COESION_HPP
+#define COESIONE_HPP
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+class Swarm {
+ private:
+  double d_k;
+  double ds_k;
+  double a_k;
+  double s_k;
+  double c_k;
+
+ public:
+  Swarm(double, double, double, double, double, double);
+  double masscenter(int n);
+}
+
+class Boid {
+  double x{};
+  double y{};
+  double v_x{};
+  double v_y{};
+
+ public:
+  std::vector<Boid> create();
+  double sumx(int n);
+}
+
+class Points {
+ private:
+  double x{};
+  double y{};
+  double v_x{};
+  double v_y{};
+
+ public:
+  double operator+(std::vector<double>){
+      return s = std::accumulate(v.begin(), v.end(), 0)};
+}
+
+#endif
