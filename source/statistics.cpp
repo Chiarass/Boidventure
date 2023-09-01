8650258efa74dd86cf6f644fb4a5a43f1f838fc7
AbyssDelver <muffinarmageddon@gmail.com>
2023-09-01 09:00:10 +0200

    seeded random engine. main build now not dependant on statistics.cpp/hpp

--- source/statistics.cpp
+++ source/statistics.cpp
@@ -8,15 +8,6 @@
 #include "point.hpp"
 
 namespace boids {
-// todo: change implementation of engine
-// random generator of values from uniform distribution, for randomly
-// generating position of boids.
-double uniform(double a, double b) {
-  // commented out for debugging
-  // std::random_device rd;
-  std::uniform_real_distribution<double> unif{a, b};
-  return unif(eng);
-}
 
 double calculate_standard_deviation(const std::vector<double>& data,
                                     double average) {
@@ -58,21 +49,5 @@ double calculate_mean_speed(const std::vector<Boid>& boid_vector) {
   double mean_speed =
       std::accumulate(speeds.begin(), speeds.end(), 0.0) / speeds.size();
   return mean_speed;
-}
-
-double approx_distance(const std::vector<boids::Boid>& boid_vector,
-                       int sample_size) {
-  // todo: assert sample size is > 0
-  std::uniform_int_distribution<> unif_int{
-      0, static_cast<int>(boid_vector.size()) - 1};
-  double approx_dist{};
-
-  for (int i = 0; i < sample_size; ++i) {
-    int k = unif_int(eng);
-    int j = unif_int(eng);
-    approx_dist += (boid_vector[k].pos() - boid_vector[j].pos()).distance();
-  }
-
-  return approx_dist / sample_size;
 }
 }  // namespace boids
--- source/statistics.hpp
+++ source/statistics.hpp
@@ -9,25 +9,12 @@
 #include "point.hpp"
 
 namespace boids {
-
-// todo: change to something passed from main. (for some techincal reason it
-// didn't work when i tried)
-inline std::mt19937 eng{};
-
-// random generator of values from uniform distribution, for randomly
-// generating position of boids. takes two doulbes, the minimum and maximum
-//of the nrange of samples it generates.
-double uniform(double, double);
-
+    
 //write some comments
 double calculate_mean_distance(const std::vector<Boid>& boid_vector);
 double calculate_mean_speed(const std::vector<Boid>& boid_vector);
 
 double calculate_standard_deviation(const std::vector<double> &, double);
-
-// function approximates average distance, using method from Barhum, Goldreich,
-// and Shraibman paper (section 1.3).
-double approx_distance(const std::vector<boids::Boid> &, int);
 }  // namespace boids
 
 #endif  // STATISTICS_HPP
